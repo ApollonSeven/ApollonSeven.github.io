5b302f574e3ad8fe1d6d32a242ad6e911d074737
Apollon Seven <apollonseven@gmail.com>
2021-04-25 17:32:59 +0300

    Add onplay and onpause animation

--- scripts/audio.js
+++ scripts/audio.js
@@ -1,4 +1,9 @@
 let audio = document.getElementById("audio");
+let audioWaves = document.getElementById("audio-waves");
 audio.onplay = () => {
     audio.classList.add("active");
+    audioWaves.classList.add("audio-waves-active")
+}
+audio.onpause = () => {
+    audioWaves.classList.remove("audio-waves-active")
 }
